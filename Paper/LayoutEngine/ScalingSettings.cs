fcc506b4ea2d75f381f76e182a7f519d84741dc4
William Chen <exanite78012@gmail.com>
2025-09-18 13:04:50 -0400

    Add constructor for ScalingSettings struct

--- Paper/LayoutEngine/ScalingSettings.cs
+++ Paper/LayoutEngine/ScalingSettings.cs
@@ -17,6 +17,9 @@ namespace Prowl.PaperUI.LayoutEngine
         /// </summary>
         public double ContentScale = 1;
 
-        public ScalingSettings() { }
+        public ScalingSettings(double contentScale = 1)
+        {
+            ContentScale = contentScale;
+        }
     }
 }
