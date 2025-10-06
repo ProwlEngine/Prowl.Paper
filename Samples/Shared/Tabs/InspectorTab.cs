3671e31c13e1ed51bc02156699d9145ee9edfc38
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2025-10-06 17:12:51 -0400

    Update InspectorTab.cs

--- Samples/Shared/Tabs/InspectorTab.cs
+++ Samples/Shared/Tabs/InspectorTab.cs
@@ -8,20 +8,7 @@ namespace Shared.Tabs
         {
             title = "Inspector";
             id = "inspector";
-            width = 83;
-        }
-
-        public override void Body()
-        {
-            using (Gui.Box("Search Box").Height(28).Margin(5).Top(8).Bottom(8).Rounded(5).BackgroundColor(Themes.base300).Enter())
-            {
-                Gui.Box("Search").Text("Filter...", Fonts.arial)
-                    .TextColor(Themes.baseContent)
-                    .Left(8)
-                    .Alignment(TextAlignment.MiddleLeft);
-            }
-
-            PaperDemo.HierarchyItem(PaperDemo.rootItem);
+            width = 70;
         }
     }
 }
